736a094101f77d5b7aa0efc30400a734fcb82030
sdzx-1 <shangdizhixia1993@163.com>
2024-08-17 14:45:28 +0800

    Refact example PingPong

--- examples/PingPong/Type.hs
+++ examples/PingPong/Type.hs
@@ -20,6 +20,7 @@
 module Type where
 
 import Control.Monad.Class.MonadSay
+import Control.Monad.Class.MonadTimer (threadDelay)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Binary
 import Data.Binary.Get
@@ -37,30 +38,23 @@ import qualified Network.Socket.ByteString as Socket
 import TypedProtocol.Codec
 import TypedProtocol.Core
 import TypedProtocol.Driver
-import Control.Monad.Class.MonadTimer (threadDelay)
 
 {-
 
------------------Client--------------Server-------------Counter-----------------
-                 (S0 s)              (S0 s)              (S2 s)
-    ---------------------------------[True]---------------------------------
-              (S0 '[True])           (S0 s)              (S2 s)
-        Ping       |       ----->      |
-                   S1                  S1                (S2 s)
-        Pong       |       <-----      |
-              (S2 '[True])            S0 s               (S2 s)
-       AddOne      |                 ----->                |
-                  S0 s                S0 s                S2 s
+-----------------------------------------------Client------------------------Server-----------------------Counter
+LABEL 0                                         S0 s                          S0 s                          S1 s
+  [Branch] Client                               S0 s                          S0 s                          S1 s
+    * BranchSt True
+    Ping                                     S0 True->                       ->S0 s                         S1 s
+    Pong                                        S2<-                          <-S2                          S1 s
+    AddOne                                   S1 True->                        S0 s                         ->S1 s
+    ^ Goto 0                                    S0 s                          S0 s                          S1 s
+    * BranchSt False
+    Stop                                     S0 False->                      ->S0 s                         S1 s
+    CStop                                    S1 False->                       End                          ->S1 s
+    ~ Terminal                                  End                           End                           End
 
-    --------------------------------[False]---------------------------------
-             (S0 '[False])           (S0 s)              (S2 s)
-        Stop       |       ----->      |
-             (S2 '[False])            End                (S2 s)
-       CStop       |                 ----->                |
-                  End                 End                 End
-                                    Terminal
- -}
-
+-}
 data Role = Client | Server | Counter
   deriving (Show, Eq, Ord)
 
@@ -68,62 +62,48 @@ data SRole :: Role -> Type where
   SClient :: SRole Client
   SServer :: SRole Server
   SCounter :: SRole Counter
-
 type instance Sing = SRole
-
 instance SingI Client where
   sing = SClient
-
 instance SingI Server where
   sing = SServer
-
 instance SingI Counter where
   sing = SCounter
-
-data PingPong
-  = S0 [Bool]
-  | S1
-  | S2 [Bool]
-  | End
-
-data SPingPong :: PingPong -> Type where
-  SS0 :: SPingPong (S0 b)
-  SS1 :: SPingPong S1
-  SS2 :: SPingPong (S2 b)
-  SEnd :: SPingPong End
-
-type instance Sing = SPingPong
-
-instance SingI (S0 b) where
-  sing = SS0
-
-instance SingI S1 where
-  sing = SS1
-
-instance SingI (S2 b) where
-  sing = SS2
-
-instance SingI End where
-  sing = SEnd
-
 instance SingToInt Role where
   singToInt x = I# (dataToTag# x)
-
-instance SingToInt PingPong where
+data PingPongSt
+  = End
+  | S0 Bool
+  | S1 Bool
+  | S2
+data SPingPongSt :: PingPongSt -> Type where
+  SEnd :: SPingPongSt End
+  SS0 :: SPingPongSt (S0 s)
+  SS1 :: SPingPongSt (S1 s)
+  SS2 :: SPingPongSt S2
+type instance Sing = SPingPongSt
+instance SingI End where
+  sing = SEnd
+instance SingI (S0 s) where
+  sing = SS0
+instance SingI (S1 s) where
+  sing = SS1
+instance SingI S2 where
+  sing = SS2
+instance SingToInt PingPongSt where
   singToInt x = I# (dataToTag# x)
-
-instance Protocol Role PingPong where
+instance Protocol Role PingPongSt where
   type Done Client = End
   type Done Server = End
   type Done Counter = End
-  data Msg Role PingPong from send recv where
-    Ping :: Msg Role PingPong (S0 '[True]) '(Client, S1) '(Server, S1)
-    Pong :: Msg Role PingPong S1 '(Server, S0 s) '(Client, S2 '[True])
-    Stop :: Msg Role PingPong (S0 '[False]) '(Client, S2 '[False]) '(Server, End)
-    AddOne :: Msg Role PingPong (S2 '[True]) '(Client, S0 s) '(Counter, S2 s)
-    CStop :: Msg Role PingPong (S2 '[False]) '(Client, End) '(Counter, End)
+  data Msg Role PingPongSt from send recv where
+    Ping :: Msg Role PingPongSt (S0 True) '(Client, S2) '(Server, S2)
+    Pong :: Msg Role PingPongSt (S2) '(Server, S0 s) '(Client, S1 True)
+    AddOne :: Msg Role PingPongSt (S1 True) '(Client, S0 s) '(Counter, S1 s)
+    Stop :: Msg Role PingPongSt (S0 False) '(Client, S1 False) '(Server, End)
+    CStop :: Msg Role PingPongSt (S1 False) '(Client, End) '(Counter, End)
 
-encodeMsg :: Encode Role PingPong L.ByteString
+encodeMsg :: Encode Role PingPongSt L.ByteString
 encodeMsg = Encode $ \x -> runPut $ case x of
   Ping -> putWord8 0
   Pong -> putWord8 1
@@ -131,7 +111,7 @@ encodeMsg = Encode $ \x -> runPut $ case x of
   AddOne -> putWord8 3
   CStop -> putWord8 4
 
-getAnyMsg :: Get (AnyMsg Role PingPong)
+getAnyMsg :: Get (AnyMsg Role PingPongSt)
 getAnyMsg = do
   v <- getWord8
   case v of
@@ -142,42 +122,42 @@ getAnyMsg = do
     4 -> pure (AnyMsg CStop)
     i -> error $ "undefined index: " ++ show i
 
-convertDecoderLBS1 ::
-  Decoder a ->
-  (DecodeStep L.ByteString CodecFailure a)
+convertDecoderLBS1
+  :: Decoder a
+  -> (DecodeStep L.ByteString CodecFailure a)
 convertDecoderLBS1 = go
-  where
-    go :: Decoder a -> DecodeStep L.ByteString CodecFailure a
-    go (Done tr _ a) = DecodeDone a (Just $ L.fromStrict tr)
-    go (Fail _ _ e) = DecodeFail (CodecFailure e)
-    go (Partial k) = DecodePartial $ \mbs -> case mbs of
-      Nothing -> go (Partial k)
-      Just _bs -> go (k $ fmap L.toStrict mbs)
+ where
+  go :: Decoder a -> DecodeStep L.ByteString CodecFailure a
+  go (Done tr _ a) = DecodeDone a (Just $ L.fromStrict tr)
+  go (Fail _ _ e) = DecodeFail (CodecFailure e)
+  go (Partial k) = DecodePartial $ \mbs -> case mbs of
+    Nothing -> go (Partial k)
+    Just _bs -> go (k $ fmap L.toStrict mbs)
 
-decodeMsg ::
-  DecodeStep
-    L.ByteString
-    CodecFailure
-    (AnyMsg Role PingPong)
+decodeMsg
+  :: DecodeStep
+      L.ByteString
+      CodecFailure
+      (AnyMsg Role PingPongSt)
 decodeMsg = convertDecoderLBS1 (runGetIncremental getAnyMsg)
 
 socketAsChannel :: Socket.Socket -> Channel IO L.ByteString
 socketAsChannel socket =
-  Channel {send, recv}
-  where
-    send :: L.ByteString -> IO ()
-    send chunks = do 
-      threadDelay 500000
-      Socket.sendMany socket (L.toChunks chunks)
+  Channel{send, recv}
+ where
+  send :: L.ByteString -> IO ()
+  send chunks = do
+    threadDelay 500000
+    Socket.sendMany socket (L.toChunks chunks)
 
-    recv :: IO (Maybe L.ByteString)
-    recv = do
-      chunk <- Socket.recv socket L.smallChunkSize
-      if BS.null chunk
-        then return Nothing
-        else return (Just (L.fromStrict chunk))
+  recv :: IO (Maybe L.ByteString)
+  recv = do
+    chunk <- Socket.recv socket L.smallChunkSize
+    if BS.null chunk
+      then return Nothing
+      else return (Just (L.fromStrict chunk))
 
-instance Show (AnyMsg Role PingPong) where
+instance Show (AnyMsg Role PingPongSt) where
   show (AnyMsg msg) = case msg of
     Ping -> "Ping"
     Pong -> "Pong"
@@ -185,21 +165,21 @@ instance Show (AnyMsg Role PingPong) where
     AddOne -> "AddOne"
     CStop -> "CStop"
 
-myTracer :: (MonadSay m) => String -> Tracer Role PingPong m
+myTracer :: (MonadSay m) => String -> Tracer Role PingPongSt m
 myTracer st v = say (st <> show v)
 
-data Choice :: PingPong -> Type where
-  ST :: Choice (S0 '[True])
-  SF :: Choice (S0 '[False])
+data Choice :: PingPongSt -> Type where
+  ST :: Choice (S0 True)
+  SF :: Choice (S0 False)
 
-choice :: (Monad m) => Int -> Peer Role PingPong Client m Choice (S0 s)
+choice :: (Monad m) => Int -> Peer Role PingPongSt Client m Choice (S0 s)
 choice i =
   if i <= 5
     then LiftM $ pure (ireturn ST)
     else LiftM $ pure (ireturn SF)
 
-clientPeer ::
-  (Monad m) => Int -> Peer Role PingPong Client m (At () (Done Client)) (S0 s)
+clientPeer
+  :: (Monad m) => Int -> Peer Role PingPongSt Client m (At () (Done Client)) (S0 s)
 clientPeer i = I.do
   res <- choice i
   case res of
@@ -212,8 +192,8 @@ clientPeer i = I.do
       yield Stop
       yield CStop
 
-serverPeer ::
-  (Monad m) => Peer Role PingPong Server m (At () (Done Server)) (S0 s)
+serverPeer
+  :: (Monad m) => Peer Role PingPongSt Server m (At () (Done Server)) (S0 s)
 serverPeer = I.do
   Recv msg <- await
   case msg of
@@ -222,8 +202,8 @@ serverPeer = I.do
       serverPeer
     Stop -> returnAt ()
 
-counterPeer ::
-  (MonadIO m) => Int -> Peer Role PingPong Counter m (At () (Done Server)) (S2 s)
+counterPeer
+  :: (MonadIO m) => Int -> Peer Role PingPongSt Counter m (At () (Done Server)) (S1 s)
 counterPeer i = I.do
   Recv msg <- await
   case msg of
