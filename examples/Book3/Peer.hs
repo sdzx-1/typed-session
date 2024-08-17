92a49d0994292af873fe84ef3c18ef9011fc262b
sdzx-1 <shangdizhixia1993@163.com>
2024-08-17 14:33:39 +0800

    Refact example Book3

--- examples/Book3/Peer.hs
+++ examples/Book3/Peer.hs
@@ -15,20 +15,20 @@
 
 module Peer where
 
-import Type
 import Control.Algebra (Has)
 import Control.Effect.Random (Random, uniform)
 import Data.IFunctor (At (..), ireturn, returnAt)
 import qualified Data.IFunctor as I
 import Data.Kind
+import Type
 import TypedProtocol.Core
 
 budget :: Int
 budget = 16
 
 data CheckPriceResult :: BookSt -> Type where
-  Yes :: CheckPriceResult (S3 [Enough, Support, Two, Found])
-  No :: CheckPriceResult (S3 [NotEnough, Support, Two, Found])
+  Yes :: CheckPriceResult (S3 Enough)
+  No :: CheckPriceResult (S3 NotEnough)
 
 checkPrice
   :: (Has Random sig m)
@@ -42,8 +42,8 @@ checkPrice _i _h = I.do
     else LiftM $ pure (ireturn No)
 
 data OT :: BookSt -> Type where
-  OTOne :: OT (S1 [One, Found])
-  OTTwo :: OT (S1 [Two, Found])
+  OTOne :: OT (S1 One)
+  OTTwo :: OT (S1 Two)
 
 choiceOT
   :: (Has Random sig m)
@@ -57,13 +57,13 @@ choiceOT _i = I.do
 
 buyerPeer
   :: (Has Random sig m)
-  => Peer Role BookSt Buyer m (At (Maybe Date) (Done Buyer)) S0
+  => Peer Role BookSt Buyer m (At (Maybe Int) (Done Buyer)) S0
 buyerPeer = I.do
   yield (Title "haskell book")
   await I.>>= \case
     Recv NoBook -> I.do
       yield SellerNoBook
-      returnAt Nothing
+      buyerPeer
     Recv (Price i) -> I.do
       choiceOT i I.>>= \case
         OTOne -> I.do
@@ -71,33 +71,33 @@ buyerPeer = I.do
           yield OneAccept
           Recv (OneDate d) <- await
           yield (OneSuccess d)
-          returnAt $ Just d
+          buyerPeer
         OTTwo -> f1
  where
   f1
     :: (Has Random sig m)
-    => Peer Role BookSt 'Buyer m (At (Maybe Date) (Done Buyer)) ('S1 '[ 'Two, 'Found])
+    => Peer Role BookSt 'Buyer m (At (Maybe Int) (Done Buyer)) ('S1 'Two)
   f1 = I.do
     yield (PriceToBuyer2 300)
     await I.>>= \case
       Recv NotSupport1 -> I.do
         yield TwoNotBuy
-        returnAt Nothing
+        buyerPeer
       Recv (SupportVal h) -> I.do
         checkPrice 10 h I.>>= \case
           Yes -> I.do
             yield TwoAccept
             Recv (TwoDate d) <- await
             yield (TwoSuccess d)
-            returnAt (Just d)
+            buyerPeer
           No -> I.do
             yield TwoNotBuy1
             yield TwoFailed
             returnAt Nothing
 
 data BuySupp :: BookSt -> Type where
-  BNS :: BuySupp (S6 '[NotSupport, Two, Found])
-  BS :: BuySupp (S6 '[Support, Two, Found])
+  BNS :: BuySupp (S6 'NotSupport)
+  BS :: BuySupp (S6 'Support)
 
 choiceB
   :: (Has Random sig m)
@@ -111,27 +111,27 @@ choiceB _i = I.do
 
 buyer2Peer
   :: (Has Random sig m)
-  => Peer Role BookSt Buyer2 m (At (Maybe Date) (Done Buyer2)) (S1 s)
+  => Peer Role BookSt Buyer2 m (At (Maybe Int) (Done Buyer2)) (S1 s)
 buyer2Peer = I.do
   await I.>>= \case
-    Recv SellerNoBook -> returnAt Nothing
+    Recv SellerNoBook -> buyer2Peer
     Recv OneAfford -> I.do
       Recv (OneSuccess d) <- await
-      returnAt (Just d)
+      buyer2Peer
     Recv (PriceToBuyer2 i) -> I.do
       choiceB i I.>>= \case
         BNS -> I.do
           yield NotSupport1
-          returnAt Nothing
+          buyer2Peer
         BS -> I.do
           yield (SupportVal (i `div` 2))
           await I.>>= \case
-            Recv (TwoSuccess d) -> returnAt $ Just d
+            Recv (TwoSuccess d) -> buyer2Peer
             Recv TwoFailed -> returnAt Nothing
 
 data FindBookResult :: BookSt -> Type where
-  NotFound' :: FindBookResult (S2 '[NotFound])
-  Found' :: FindBookResult (S2 '[Found])
+  NotFound' :: FindBookResult (S2 'NotFound)
+  Found' :: FindBookResult (S2 'Found)
 
 findBook
   :: (Has Random sig m)
@@ -149,11 +149,17 @@ sellerPeer
 sellerPeer = I.do
   Recv (Title st) <- await
   findBook st I.>>= \case
-    NotFound' -> yield NoBook
+    NotFound' -> I.do
+      yield NoBook
+      sellerPeer
     Found' -> I.do
       yield (Price 30)
       await I.>>= \case
-        Recv OneAccept -> yield (OneDate 100)
-        Recv TwoNotBuy -> returnAt ()
-        Recv TwoAccept -> yield (TwoDate 100)
+        Recv OneAccept -> I.do
+          yield (OneDate 100)
+          sellerPeer
+        Recv TwoNotBuy -> sellerPeer
+        Recv TwoAccept -> I.do
+          yield (TwoDate 100)
+          sellerPeer
         Recv TwoNotBuy1 -> returnAt ()
--- examples/Book3/Type.hs
+++ examples/Book3/Type.hs
@@ -6,12 +6,12 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE QualifiedDo #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE NamedFieldPuns #-}
 
 module Type where
 
@@ -20,101 +20,53 @@ import qualified Data.IFunctor as I
 import Data.Kind
 import GHC.Exts (dataToTag#)
 import GHC.Int (Int (I#))
-import TypedProtocol.Codec
 import TypedProtocol.Core
-import qualified Data.ByteString.Lazy as L
-import Data.Binary
-import Data.Binary.Put
-import Data.Binary.Get
-import qualified Network.Socket as Socket
-import qualified Network.Socket.ByteString as Socket
-import qualified Data.ByteString.Builder.Extra as L
-import qualified Data.ByteString as BS
-
 
 {-
----------------------------Buyer-------------------------Seller------------------------Buyer2---------------------------
-                             S0                            S0                          (S1 s)
-            Title            |            ----->           |
-                           (S2 s)                        (S2 s)                        (S1 s)
-    ---------------------------------------------------[NotFound]---------------------------------------------------
-                           (S2 s)                   (S2 [NotFound])                    (S1 s)
-            NoBook           |            <-----           |
-                      (S1 [NotFound])                     End                          (S1 s)
-         SellerNoBook        |                           ----->                          |
-                            End                           End                           End
-                                                        Terminal
-
-    ----------------------------------------------------[Found]-----------------------------------------------------
-                           (S2 s)                     (S2 [Found])                     (S1 s)
-            Price            |            <-----           |
-                           (S1 s)                        (S3 s)                        (S1 s)
-        ----------------------------------------------[One,Found]-----------------------------------------------
-                      (S1 [One,Found])                   (S3 s)                        (S1 s)
-          OneAfford          |                           ----->                          |
-                      (S3 [One,Found])                   (S3 s)                          S4
-          OneAccept          |            ----->           |
-                             S5                            S5                            S4
-           OneDate           |            <-----           |
-                             S4                           End                            S4
-          OneSuccess         |                           ----->                          |
-                            End                           End                           End
-                                                        Terminal
-
-        ----------------------------------------------[Two,Found]-----------------------------------------------
-                      (S1 [Two,Found])                   (S3 s)                        (S1 s)
-        PriceToBuyer2        |                           ----->                          |
-                           (S6 s)                        (S3 s)                        (S6 s)
-            -------------------------------------[NotSupport,Two,Found]-------------------------------------
-                           (S6 s)                        (S3 s)             (S6 [NotSupport,Two,Found])
-          NotSupport1        |                           <-----                          |
-                (S3 [NotSupport,Two,Found])              (S3 s)                         End
-          TwoNotBuy          |            ----->           |
-                            End                           End                           End
-                                                        Terminal
-
-            --------------------------------------[Support,Two,Found]---------------------------------------
-                           (S6 s)                        (S3 s)               (S6 [Support,Two,Found])
-          SupportVal         |                           <-----                          |
-                           (S3 s)                        (S3 s)                        (S7 s)
-                -------------------------------[Enough,Support,Two,Found]-------------------------------
-              (S3 [Enough,Support,Two,Found])            (S3 s)                        (S7 s)
-          TwoAccept          |            ----->           |
-                             S8                            S8                          (S7 s)
-           TwoDate           |            <-----           |
-              (S7 [Enough,Support,Two,Found])             End                          (S7 s)
-          TwoSuccess         |                           ----->                          |
-                            End                           End                           End
-                                                        Terminal
-
-                -----------------------------[NotEnough,Support,Two,Found]------------------------------
-             (S3 [NotEnough,Support,Two,Found])          (S3 s)                        (S7 s)
-          TwoNotBuy1         |            ----->           |
-             (S7 [NotEnough,Support,Two,Found])           End                          (S7 s)
-          TwoFailed          |                           ----->                          |
-                            End                           End                           End
-                                                        Terminal
+-----------------------------------------------Buyer-------------------------Seller------------------------Buyer2
+LABEL 0                                          S0                            S0                           S1 s
+  Title                                         S0->                          ->S0                          S1 s
+  [Branch] Seller                               S2 s                          S2 s                          S1 s
+    * BranchSt NotFound
+    NoBook                                     S2 s<-                    <-S2 NotFound                      S1 s
+    SellerNoBook                           S1 NotFound->                       S0                          ->S1 s
+    ^ Goto 0                                     S0                            S0                           S1 s
+    * BranchSt Found
+    Price                                      S2 s<-                      <-S2 Found                       S1 s
+    [Branch] Buyer                              S1 s                          S3 s                          S1 s
+      * BranchSt One
+      OneAfford                               S1 One->                        S3 s                         ->S1 s
+      OneAccept                               S3 One->                       ->S3 s                          S4
+      OneDate                                   S5<-                          <-S5                           S4
+      OneSuccess                                S4->                           S0                           ->S4
+      ^ Goto 0                                   S0                            S0                           S1 s
+      * BranchSt Two
+      PriceToBuyer2                           S1 Two->                        S3 s                         ->S1 s
+      [Branch] Buyer2                           S6 s                          S3 s                          S6 s
+        * BranchSt NotSupport
+        NotSupport1                            S6 s<-                         S3 s                    <-S6 NotSupport
+        TwoNotBuy                         S3 NotSupport->                    ->S3 s                         S1 s
+        ^ Goto 0                                 S0                            S0                           S1 s
+        * BranchSt Support
+        SupportVal                             S6 s<-                         S3 s                      <-S6 Support
+        [Branch] Buyer                          S3 s                          S3 s                          S7 s
+          * BranchSt Enough
+          TwoAccept                         S3 Enough->                      ->S3 s                         S7 s
+          TwoDate                               S8<-                          <-S8                          S7 s
+          TwoSuccess                        S7 Enough->                        S0                          ->S7 s
+          ^ Goto 0                               S0                            S0                           S1 s
+          * BranchSt NotEnough
+          TwoNotBuy1                       S3 NotEnough->                    ->S3 s                         S7 s
+          TwoFailed                        S7 NotEnough->                     End                          ->S7 s
+          ~ Terminal                            End                           End                           End
 
 -}
 
-data Role = Buyer | Seller | Buyer2
-  deriving (Show, Eq, Ord)
-
-data SRole :: Role -> Type where
-  SBuyer :: SRole Buyer
-  SBuyer2 :: SRole Buyer2
-  SSeller :: SRole Seller
-
-type instance Sing = SRole
-
-instance SingI Buyer where
-  sing = SBuyer
-
-instance SingI Buyer2 where
-  sing = SBuyer2
-
-instance SingI Seller where
-  sing = SSeller
+data Role
+  = Buyer
+  | Seller
+  | Buyer2
+  deriving (Show, Eq, Ord, Enum, Bounded)
 
 data BookBranchSt
   = NotFound
@@ -127,19 +79,32 @@ data BookBranchSt
   | NotEnough
   deriving (Show)
 
+data SRole :: Role -> Type where
+  SBuyer :: SRole Buyer
+  SSeller :: SRole Seller
+  SBuyer2 :: SRole Buyer2
+type instance Sing = SRole
+instance SingI Buyer where
+  sing = SBuyer
+instance SingI Seller where
+  sing = SSeller
+instance SingI Buyer2 where
+  sing = SBuyer2
+instance SingToInt Role where
+  singToInt x = I# (dataToTag# x)
 data BookSt
-  = S0
-  | S1 [BookBranchSt]
-  | S2 [BookBranchSt]
-  | S3 [BookBranchSt]
+  = End
+  | S0
+  | S1 BookBranchSt
+  | S2 BookBranchSt
+  | S3 BookBranchSt
   | S4
   | S5
-  | S6 [BookBranchSt]
-  | S7 [BookBranchSt]
+  | S6 BookBranchSt
+  | S7 BookBranchSt
   | S8
-  | End
-
 data SBookSt :: BookSt -> Type where
+  SEnd :: SBookSt End
   SS0 :: SBookSt S0
   SS1 :: SBookSt (S1 s)
   SS2 :: SBookSt (S2 s)
@@ -149,71 +114,51 @@ data SBookSt :: BookSt -> Type where
   SS6 :: SBookSt (S6 s)
   SS7 :: SBookSt (S7 s)
   SS8 :: SBookSt S8
-  SEnd :: SBookSt End
-
 type instance Sing = SBookSt
-
-instance SingI S0 where
-  sing = SS0
-
-instance SingI (S1 s) where
-  sing = SS1
-
-instance SingI (S2 s) where
-  sing = SS2
-
-instance SingI (S3 s) where
-  sing = SS3
-
-instance SingI S4 where
-  sing = SS4
-
-instance SingI S5 where
-  sing = SS5
-
-instance SingI (S6 s) where
-  sing = SS6
-
-instance SingI (S7 s) where
-  sing = SS7
-
-instance SingI S8 where
-  sing = SS8
-
 instance SingI End where
   sing = SEnd
-
-type Date = Int
-
-instance SingToInt Role where
-  singToInt x = I# (dataToTag# x)
-
+instance SingI S0 where
+  sing = SS0
+instance SingI (S1 s) where
+  sing = SS1
+instance SingI (S2 s) where
+  sing = SS2
+instance SingI (S3 s) where
+  sing = SS3
+instance SingI S4 where
+  sing = SS4
+instance SingI S5 where
+  sing = SS5
+instance SingI (S6 s) where
+  sing = SS6
+instance SingI (S7 s) where
+  sing = SS7
+instance SingI S8 where
+  sing = SS8
 instance SingToInt BookSt where
   singToInt x = I# (dataToTag# x)
-
 instance Protocol Role BookSt where
   type Done Buyer = End
   type Done Seller = End
   type Done Buyer2 = End
-
   data Msg Role BookSt from send recv where
-    Title :: String -> Msg Role BookSt S0 '(Buyer, S2 s) '(Seller, S2 s)
-    NoBook :: Msg Role BookSt (S2 '[NotFound]) '(Seller, End) '(Buyer, S1 '[NotFound])
-    SellerNoBook :: Msg Role BookSt (S1 '[NotFound]) '(Buyer, End) '(Buyer2, End)
-    Price :: Int -> Msg Role BookSt (S2 '[Found]) '(Seller, S3 s) '(Buyer, S1 s)
-    OneAfford :: Msg Role BookSt (S1 '[One, Found]) '(Buyer, S3 '[One, Found]) '(Buyer2, S4)
-    OneAccept :: Msg Role BookSt (S3 '[One, Found]) '(Buyer, S5) '(Seller, S5)
-    OneDate :: Date -> Msg Role BookSt S5 '(Seller, End) '(Buyer, S4)
-    OneSuccess :: Date -> Msg Role BookSt S4 '(Buyer, End) '(Buyer2, End)
-    PriceToBuyer2 :: Int -> Msg Role BookSt (S1 '[Two, Found]) '(Buyer, S6 s) '(Buyer2, S6 s)
-    NotSupport1 :: Msg Role BookSt (S6 '[NotSupport, Two, Found]) '(Buyer2, End) '(Buyer, S3 '[NotSupport, Two, Found])
-    TwoNotBuy :: Msg Role BookSt (S3 '[NotSupport, Two, Found]) '(Buyer, End) '(Seller, End)
-    SupportVal :: Int -> Msg Role BookSt (S6 '[Support, Two, Found]) '(Buyer2, S7 s) '(Buyer, S3 s)
-    TwoAccept :: Msg Role BookSt (S3 '[Enough, Support, Two, Found]) '(Buyer, S8) '(Seller, S8)
-    TwoDate :: Date -> Msg Role BookSt S8 '(Seller, End) '(Buyer, S7 '[Enough, Support, Two, Found])
-    TwoSuccess :: Date -> Msg Role BookSt (S7 '[Enough, Support, Two, Found]) '(Buyer, End) '(Buyer2, End)
-    TwoNotBuy1 :: Msg Role BookSt (S3 '[NotEnough, Support, Two, Found]) '(Buyer, S7 '[NotEnough, Support, Two, Found]) '(Seller, End)
-    TwoFailed :: Msg Role BookSt (S7 '[NotEnough, Support, Two, Found]) '(Buyer, End) '(Buyer2, End)
+    Title :: String -> Msg Role BookSt (S0) '(Buyer, S2 s) '(Seller, S2 s)
+    NoBook :: Msg Role BookSt (S2 NotFound) '(Seller, S0) '(Buyer, S1 NotFound)
+    SellerNoBook :: Msg Role BookSt (S1 NotFound) '(Buyer, S0) '(Buyer2, S1 s)
+    Price :: Int -> Msg Role BookSt (S2 Found) '(Seller, S3 s) '(Buyer, S1 s)
+    OneAfford :: Msg Role BookSt (S1 One) '(Buyer, S3 One) '(Buyer2, S4)
+    OneAccept :: Msg Role BookSt (S3 One) '(Buyer, S5) '(Seller, S5)
+    OneDate :: Int -> Msg Role BookSt (S5) '(Seller, S0) '(Buyer, S4)
+    OneSuccess :: Int -> Msg Role BookSt (S4) '(Buyer, S0) '(Buyer2, S1 s)
+    PriceToBuyer2 :: Int -> Msg Role BookSt (S1 Two) '(Buyer, S6 s) '(Buyer2, S6 s)
+    NotSupport1 :: Msg Role BookSt (S6 NotSupport) '(Buyer2, S1 s) '(Buyer, S3 NotSupport)
+    TwoNotBuy :: Msg Role BookSt (S3 NotSupport) '(Buyer, S0) '(Seller, S0)
+    SupportVal :: Int -> Msg Role BookSt (S6 Support) '(Buyer2, S7 s) '(Buyer, S3 s)
+    TwoAccept :: Msg Role BookSt (S3 Enough) '(Buyer, S8) '(Seller, S8)
+    TwoDate :: Int -> Msg Role BookSt (S8) '(Seller, S0) '(Buyer, S7 Enough)
+    TwoSuccess :: Int -> Msg Role BookSt (S7 Enough) '(Buyer, S0) '(Buyer2, S1 s)
+    TwoNotBuy1 :: Msg Role BookSt (S3 NotEnough) '(Buyer, S7 NotEnough) '(Seller, End)
+    TwoFailed :: Msg Role BookSt (S7 NotEnough) '(Buyer, End) '(Buyer2, End)
 
 instance Show (AnyMsg Role BookSt) where
   show (AnyMsg msg) = case msg of
