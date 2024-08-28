9c0dc30c669a51063051e2d60f9397bb093ba1e5
sdzx-1 <shangdizhixia1993@163.com>
2024-08-28 10:08:38 +0800

    refactor TH

--- src/TypedSession/TH.hs
+++ src/TypedSession/TH.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MagicHash #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
@@ -17,11 +18,15 @@ module TypedSession.TH where
 
 import Control.Monad (forM)
 import Data.Either (fromRight)
+import Data.IFunctor (Sing, SingI (..))
 import Data.Kind
 import qualified Data.Set as Set
+import GHC.Exts (DataToTag (..), Int (..))
 import Language.Haskell.TH hiding (Type)
 import qualified Language.Haskell.TH as TH
 import Language.Haskell.TH.Quote
+import TypedSession.Core (SingToInt (..))
+import qualified TypedSession.Core as TSC
 import TypedSession.State.Parser (runProtocolParser)
 import TypedSession.State.Pipeline (PipeResult (..), genGraph, pipe)
 import TypedSession.State.Render
@@ -32,46 +37,51 @@ roleDecs name = do
   res <- reify name
   case res of
     TyConI (DataD [] dName [] Nothing cons _) -> do
-      a <- newName "a"
-      x <- newName "x"
-      pure $
-        [ DataD
-            []
-            (addS dName)
-            [KindedTV a BndrReq (ConT dName)]
-            Nothing
-            [GadtC [addS n] [] (AppT (ConT (addS dName)) (PromotedT n)) | NormalC n [] <- cons]
-            []
-        , TySynInstD (TySynEqn Nothing (ConT (mkName "Data.IFunctor.Sing")) (ConT (addS dName)))
-        ]
-          ++ [ InstanceD
-                Nothing
-                []
-                (AppT (ConT $ mkName "Data.IFunctor.SingI") (PromotedT n))
-                [FunD (mkName "sing") [Clause [] (NormalB (ConE (addS n))) []]]
-             | NormalC n [] <- cons
-             ]
-          ++ [ InstanceD
-                Nothing
-                []
-                (AppT (ConT (mkName "TypedSession.Core.SingToInt")) (ConT name))
-                [ FunD
-                    (mkName "singToInt")
-                    [Clause [VarP x] (NormalB (AppE (ConE (mkName "I#")) (AppE (VarE $ mkName "dataToTag#") (VarE x)))) []]
-                ]
-             ]
-    _ -> error "np"
+      aVar <- newName "a"
+      xVar <- newName "x"
+      let
+        addPerfixS :: Name -> Name
+        addPerfixS vname =
+          let n = (nameBase vname)
+           in mkName ("S" <> n)
 
-addS :: Name -> Name
-addS name =
-  let n = (nameBase name)
-   in mkName ("S" <> n)
+        roleSingleton =
+          [ DataD
+              []
+              (addPerfixS dName)
+              [KindedTV aVar BndrReq (ConT dName)]
+              Nothing
+              [GadtC [addPerfixS n] [] (AppT (ConT (addPerfixS dName)) (PromotedT n)) | NormalC n [] <- cons]
+              []
+          ]
+        singToSRole = [TySynInstD (TySynEqn Nothing (ConT ''Sing) (ConT (addPerfixS dName)))]
+        instanceSingI =
+          [ InstanceD
+              Nothing
+              []
+              (AppT (ConT ''SingI) (PromotedT n))
+              [FunD 'sing [Clause [] (NormalB (ConE (addPerfixS n))) []]]
+          | NormalC n [] <- cons
+          ]
+        instanceSingToInt =
+          [ InstanceD
+              Nothing
+              []
+              (AppT (ConT ''SingToInt) (ConT name))
+              [ FunD
+                  'singToInt
+                  [Clause [VarP xVar] (NormalB (AppE (ConE 'I#) (AppE (VarE 'dataToTag#) (VarE xVar)))) []]
+              ]
+          ]
+      pure $ roleSingleton ++ singToSRole ++ instanceSingI ++ instanceSingToInt
+    _ -> error $ "Name: " ++ show name ++ " is not a data constructor"
 
 protDecsAndMsgDecs :: forall r bst. (Show r, Show bst) => String -> Name -> Name -> PipeResult r bst -> Q [Dec]
 protDecsAndMsgDecs protN roleName bstName PipeResult{msgT1, dnySet, stBound = (fromVal, toVal)} = do
-  sVar <- newName "s"
   let protName = mkName protN
       protSName = mkName ("S" <> protN)
+      mkSiName i = mkName $ "S" <> show i
+      mkSSiName i = mkName $ "SS" <> show i
       genConstr i =
         if i == -1
           then NormalC (mkName "End") []
@@ -79,10 +89,15 @@ protDecsAndMsgDecs protN roleName bstName PipeResult{msgT1, dnySet, stBound = (f
             if i `Set.member` dnySet
               then
                 NormalC
-                  (mkName $ "S" <> show i)
+                  (mkSiName i)
                   [(Bang NoSourceUnpackedness NoSourceStrictness, ConT bstName)]
-              else NormalC (mkName $ "S" <> show i) []
-      genSConstr i =
+              else NormalC (mkSiName i) []
+      -- generate protocol data type
+      dataProt = [DataD [] protName [] Nothing [genConstr i | i <- [fromVal .. toVal]] []]
+
+  sVar <- newName "s"
+  aVar <- newName "a"
+  let genSConstr i =
         if i == -1
           then
             GadtC [mkName "SEnd"] [] (AppT (ConT protSName) (PromotedT (mkName "End")))
@@ -93,45 +108,89 @@ protDecsAndMsgDecs protN roleName bstName PipeResult{msgT1, dnySet, stBound = (f
                   [KindedTV sVar SpecifiedSpec (ConT bstName)]
                   []
                   ( GadtC
-                      [mkName $ "SS" <> show i]
+                      [mkSSiName i]
                       []
-                      (AppT (ConT protSName) (AppT (PromotedT $ mkName $ "S" <> show i) (VarT sVar)))
+                      (AppT (ConT protSName) (AppT (PromotedT $ mkSiName i) (VarT sVar)))
                   )
               else
-                GadtC [mkName $ "SS" <> show i] [] (AppT (ConT protSName) (PromotedT $ mkName $ "S" <> show i))
+                GadtC [mkSSiName i] [] (AppT (ConT protSName) (PromotedT $ mkSiName i))
+      -- generate protocol singleton data type
+      dataSingletonProt = [DataD [] protSName [KindedTV aVar BndrReq (ConT protName)] Nothing [genSConstr i | i <- [fromVal .. toVal]] []]
+  -- generate type family Sing to Singleton protocol
+  let singSingletonProt = [TySynInstD (TySynEqn Nothing (ConT ''Sing) (ConT protSName))]
+
+  s1 <- newName "s1"
+  -- generate instance SingI
+  let instanceSingI =
+        [ InstanceD
+            Nothing
+            []
+            ( AppT
+                (ConT ''SingI)
+                ( if i == -1
+                    then PromotedT (mkName "End")
+                    else
+                      if i `Set.member` dnySet
+                        then SigT (AppT (PromotedT (mkSiName i)) (VarT s1)) (ConT protName)
+                        else PromotedT (mkSiName i)
+                )
+            )
+            [FunD 'sing [Clause [] (NormalB (ConE (mkName $ "S" <> (if i == -1 then "End" else ("S" <> show i))))) []]]
+        | i <- [fromVal .. toVal]
+        ]
+
+  xVar <- newName "x"
+  -- generate instance SingToInt
+  let instanceSingToInt =
+        [ InstanceD
+            Nothing
+            []
+            (AppT (ConT ''SingToInt) (ConT protName))
+            [ FunD 'singToInt [Clause [VarP xVar] (NormalB (AppE (ConE 'I#) (AppE (VarE 'dataToTag#) (VarE xVar)))) []]
+            ]
+        ]
+
+  -- make instance type family Done
+  roleRes <- reify roleName
+  instDoneDesc <- case roleRes of
+    TyConI (DataD [] _ [] Nothing cons _) -> do
+      pure
+        [ TySynInstD
+            ( TySynEqn
+                Nothing
+                (AppT (ConT (mkName "Done")) (PromotedT n))
+                (PromotedT (mkName "End"))
+            )
+        | NormalC n [] <- cons
+        ]
+    _ -> error $ "Name: " ++ show roleName ++ " is not a data constructor"
+
+  let typeListT :: [TH.Type] -> TH.Type
+      typeListT = foldl1 AppT
 
       isTAny :: T bst -> Bool
       isTAny = \case
         TAny _ -> True
         _ -> False
 
-      mkInstanceMsg :: Name -> Protocol (MsgT1 r bst) r bst -> Q [Con]
-      mkInstanceMsg s = \case
+      mkDataInstanceMsg :: Name -> Protocol (MsgT1 r bst) r bst -> Q [Con]
+      mkDataInstanceMsg s = \case
         Msg ((a, b, c), (from, to), _) constr args _ _ :> prots -> do
           let tAnyToType :: T bst -> TH.Type
               tAnyToType = \case
-                TNum i -> PromotedT (mkName $ "S" <> show i)
-                BstList i bst -> AppT (PromotedT (mkName $ "S" <> show i)) (PromotedT (mkName (show bst)))
-                TAny i -> AppT (PromotedT (mkName $ "S" <> show i)) (VarT s)
+                TNum i -> PromotedT (mkSiName i)
+                BstList i bst -> AppT (PromotedT (mkSiName i)) (PromotedT (mkName (show bst)))
+                TAny i -> AppT (PromotedT (mkSiName i)) (VarT s)
                 TEnd -> PromotedT $ mkName "End"
-
           let mkTName =
-                ( AppT
-                    ( AppT
-                        ( AppT
-                            ( AppT
-                                ( AppT
-                                    (ConT $ mkName "Msg")
-                                    (ConT roleName)
-                                )
-                                (ConT protName)
-                            )
-                            (tAnyToType a)
-                        )
-                        (AppT (AppT (PromotedTupleT 2) (PromotedT (mkName (show from)))) (tAnyToType b))
-                    )
-                    (AppT (AppT (PromotedTupleT 2) (PromotedT (mkName (show to)))) (tAnyToType c))
-                )
+                typeListT
+                  [ ConT ''TSC.Msg
+                  , ConT roleName
+                  , ConT protName
+                  , tAnyToType a
+                  , typeListT [PromotedTupleT 2, PromotedT (mkName (show from)), tAnyToType b]
+                  , typeListT [PromotedTupleT 2, PromotedT (mkName (show to)), tAnyToType c]
+                  ]
           let val =
                 let gadtc =
                       GadtC
@@ -145,86 +204,27 @@ protDecsAndMsgDecs protN roleName bstName PipeResult{msgT1, dnySet, stBound = (f
                         ]
                         mkTName
                  in if any isTAny [a, b, c]
-                      then
-                        ForallC
-                          [KindedTV s SpecifiedSpec (ConT bstName)]
-                          []
-                          gadtc
+                      then ForallC [KindedTV s SpecifiedSpec (ConT bstName)] [] gadtc
                       else gadtc
-          res <- mkInstanceMsg s prots
-          pure (val : res)
-        Label _ _ :> prots -> mkInstanceMsg s prots
+          vals <- mkDataInstanceMsg s prots
+          pure (val : vals)
+        Label _ _ :> prots -> mkDataInstanceMsg s prots
         Branch _ _ ls -> do
-          ls' <- forM ls $ \(BranchSt _ _ prot) -> mkInstanceMsg s prot
+          ls' <- forM ls $ \(BranchSt _ _ prot) -> mkDataInstanceMsg s prot
           pure $ concat ls'
         _ -> pure []
 
-  a <- newName "a"
-  s1 <- newName "s1"
-  x <- newName "x"
-
-  -- make instance Done
-  res <- reify roleName
-  instDoneDesc <- case res of
-    TyConI (DataD [] _ [] Nothing cons _) -> do
-      pure
-        [ TySynInstD
-            ( TySynEqn
-                Nothing
-                (AppT (ConT (mkName "Done")) (PromotedT n))
-                (PromotedT (mkName "End"))
-            )
-        | NormalC n [] <- cons
-        ]
-    _ -> error "np"
-
-  -- make instance msg
-  ss <- newName "s"
-  instMsgDesc <- mkInstanceMsg ss (msgT1)
-
+  -- make data instance Msg
+  ssVar <- newName "s"
+  instMsgDesc <- mkDataInstanceMsg ssVar (msgT1)
   fromVar <- newName "from"
   sendVar <- newName "send"
   recvVar <- newName "recv"
-  pure $
-    [ DataD [] protName [] Nothing [genConstr i | i <- [fromVal .. toVal]] []
-    ]
-      ++ [DataD [] protSName [KindedTV a BndrReq (ConT protName)] Nothing [genSConstr i | i <- [fromVal .. toVal]] []]
-      ++ [TySynInstD (TySynEqn Nothing (ConT (mkName "Data.IFunctor.Sing")) (ConT protSName))]
-      ++ [ InstanceD
+  let instanceMsg =
+        [ InstanceD
             Nothing
             []
-            ( AppT
-                (ConT $ mkName "Data.IFunctor.SingI")
-                ( if i == -1
-                    then PromotedT (mkName "End")
-                    else
-                      if i `Set.member` dnySet
-                        then SigT (AppT (PromotedT (mkName ("S" <> show i))) (VarT s1)) (ConT protName)
-                        else PromotedT (mkName ("S" <> show i))
-                )
-            )
-            [ FunD
-                (mkName "sing")
-                [Clause [] (NormalB (ConE (mkName $ "S" <> (if i == -1 then "End" else ("S" <> show i))))) []]
-            ]
-         | i <- [fromVal .. toVal]
-         ]
-      ++ [ InstanceD
-            Nothing
-            []
-            (AppT (ConT (mkName "TypedSession.Core.SingToInt")) (ConT protName))
-            [ FunD
-                (mkName "singToInt")
-                [Clause [VarP x] (NormalB (AppE (ConE (mkName "I#")) (AppE (VarE $ mkName "dataToTag#") (VarE x)))) []]
-            ]
-         ]
-      ++ [ InstanceD
-            Nothing
-            []
-            ( AppT
-                (AppT (ConT (mkName "TypedSession.Core.Protocol")) (ConT roleName))
-                (ConT protName)
-            )
+            (AppT (AppT (ConT ''TSC.Protocol) (ConT roleName)) (ConT protName))
             ( instDoneDesc
                 ++ let ct1 = (AppT (AppT (TupleT 2) (ConT roleName)) (ConT protName))
                     in [ DataInstD
@@ -235,28 +235,29 @@ protDecsAndMsgDecs protN roleName bstName PipeResult{msgT1, dnySet, stBound = (f
                               , KindedTV recvVar () ct1
                               ]
                           )
-                          ( AppT
-                              ( AppT
-                                  ( AppT
-                                      ( AppT
-                                          ( AppT
-                                              (ConT $ mkName "Msg")
-                                              (ConT roleName)
-                                          )
-                                          (ConT protName)
-                                      )
-                                      (SigT (VarT fromVar) (ConT protName))
-                                  )
-                                  (SigT (VarT sendVar) ct1)
-                              )
-                              (SigT (VarT recvVar) ct1)
+                          ( typeListT
+                              [ ConT ''TSC.Msg
+                              , ConT roleName
+                              , ConT protName
+                              , (SigT (VarT fromVar) (ConT protName))
+                              , (SigT (VarT sendVar) ct1)
+                              , (SigT (VarT recvVar) ct1)
+                              ]
                           )
                           Nothing
                           instMsgDesc
                           []
                        ]
             )
-         ]
+        ]
+
+  pure $
+    dataProt
+      ++ dataSingletonProt
+      ++ singSingletonProt
+      ++ instanceSingI
+      ++ instanceSingToInt
+      ++ instanceMsg
 
 protocol
   :: forall r bst
